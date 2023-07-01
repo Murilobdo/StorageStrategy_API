0078e304df3eb5b22bf5b925bd52a0dd30ee84a8
Murilobdo <murilobdo@hotmail.com>
2023-07-01 12:18:14 -0300

    Removendo a condição do swagger no program.cs

--- StorageStrategy.API/Program.cs
+++ StorageStrategy.API/Program.cs
@@ -32,11 +32,8 @@ builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
