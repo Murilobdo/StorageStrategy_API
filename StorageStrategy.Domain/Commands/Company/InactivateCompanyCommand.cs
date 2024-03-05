0213b96a820159dab9539218c0e6bbeb1b9a754f
= <murilobdo@hotmail.com>
2024-03-05 00:15:52 -0300

    feature: logica para inativar a empresa e renovar a licenca da mesma

---
+++ StorageStrategy.Domain/Commands/Company/InactivateCompanyCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using StorageStrategy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageStrategy.Domain.Commands.Company
+{
+    public record InactivateCompanyCommand(int CompanyId) : IRequest<Result>
+    {
+        public int CompanyId { get; set; } = CompanyId;
+    }
+}
---
+++ StorageStrategy.Domain/Commands/Company/RenovateCompanyCommand.cs
@@ -0,0 +1,12 @@
+﻿
+using MediatR;
+using StorageStrategy.Models;
+
+namespace StorageStrategy.Domain.Commands.Company
+{
+    public record class RenovateCompanyCommand(int CompanyId, string NewDate) : IRequest<Result>
+    {
+        public int CompanyId { get; set; } = CompanyId;
+        public string NewDate { get; set; } = NewDate;
+    }
+}
