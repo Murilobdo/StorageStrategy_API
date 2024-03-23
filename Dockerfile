d3184c5b8c93c87e05f3f39c909c7fa7a5bb9746
= <murilobdo@hotmail.com>
2024-03-23 13:55:12 -0300

    feature: configuração Docker finalizada incluindo banco de dados

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
+
+WORKDIR /App
+
+COPY . .
+
+RUN dotnet restore
+
+RUN dotnet clean
+
+RUN dotnet test StorageStrategy.Tests/StorageStrategy.Tests.csproj
+
+RUN dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS runtime
+
+WORKDIR /App
+
+COPY --from=build /App/out /App/
+
+EXPOSE 8080
+
+CMD [ "dotnet", "StorageStrategy.API.dll"]
---
+++ StorageStrategy.Domain/Validations/NFE/NFCeValidation.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using StorageStrategy.Domain.Commands.NFE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageStrategy.Domain.Validations.NFE
+{
+    public class NFCeValidation : AbstractValidator<CreateNFCommand>
+    {
+        public NFCeValidation()
+        {
+            ValidationCompanyId();
+        }
+
+        protected void ValidationCompanyId() => RuleFor(p => p.CompanyId).NotEmpty();
+    }
+}
