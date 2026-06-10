930cd05eb5a9f9f81c317325cb6af17e2067f19b
unknown <murilobdo@hotmail.com>
2026-06-10 15:49:44 -0300

    HOTFIX: REMOVENDO A UI DO SWAGGER EM PRODUÇÃO

--- StorageStrategy.API/Program.cs
+++ StorageStrategy.API/Program.cs
@@ -38,8 +38,12 @@ builder.Host.UseDefaultServiceProvider(cfg =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
